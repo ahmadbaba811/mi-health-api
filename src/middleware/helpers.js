61f75acee927983fba4797c87ca7ad2965e5bce7
Ahmad Idris <ahmadbaba811@hotmail.com>
2026-06-16 02:05:50 +0100

    added search page

--- src/middleware/helpers.js
+++ src/middleware/helpers.js
@@ -1,4 +1,7 @@
 
+const { pool, sql } = require('../db');
+const Fuse = require("fuse.js");
+
 const formatDate = (dateString) => {
     return new Date(dateString)
         .toISOString()
@@ -11,4 +14,48 @@ const formatTime = (dateString) => {
         .substring(11, 16);
 };
 
-module.exports = { formatTime, formatDate }
+async function findClosestSearch(search) {
+    const candidates = [];
+
+    // Services
+    const services = await pool.request().query(`SELECT Name FROM lk_services`);
+
+    services.recordset.forEach(row =>
+        candidates.push(row.Name)
+    );
+
+    // Labs
+    const labs = await pool.request().query(`SELECT Name FROM Labs`);
+
+    labs.recordset.forEach(row =>
+        candidates.push(row.Name)
+    );
+
+    // States & LGAs
+    const locations = await pool.request().query(`
+        SELECT DISTINCT state, lga
+        FROM Labs
+    `);
+
+    locations.recordset.forEach(row => {
+        if (row.state) candidates.push(row.state);
+        if (row.lga) candidates.push(row.lga);
+    });
+
+    const fuse = new Fuse(candidates, {
+        threshold: 0.4,
+        ignoreLocation: true,
+        includeScore: true
+    });
+
+    const bestMatch = fuse.search(search);
+
+    
+    if (bestMatch.length === 0 || bestMatch.score > 4) {
+        return null;
+    }
+
+    return bestMatch[0];
+}
+
+module.exports = { formatTime, formatDate, findClosestSearch }
